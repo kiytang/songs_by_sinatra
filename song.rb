b06e47a4deb45c3bfcad0053af8ba3b48e8d66ae
Kiy Hong Tang <kiyhong@gmail.com>
2014-02-20 20:17:10 +0000

    added views template to edit songs

--- song.rb
+++ song.rb
@@ -13,10 +13,15 @@ class Song
 	property :lyrics,   	 Text
 	property :length, 		 Integer
 	property :released_on, Date
+
+	def released_on=date
+		super Date.strptime(date, '%m/%d/%Y')
+	end
 end
 
 DataMapper.finalize 
-
+# this route handler creates an empty song object and then displays the 
+# new_song view
 get '/songs/new' do
 	@song = Song.new
 	slim :new_song
@@ -27,4 +32,18 @@ get '/songs/:id' do
 	slim :show_song
 end
 
+post '/songs' do
+	song = Song.create(params[:song])
+	redirect to("/songs/#{song.id}")
+end
 
+get '/songs/:id/edit' do
+	@song = Song.get(params[:id])
+	slim :edit_song
+end
+
+put '/songs/:id' do
+	song = Song.get(params[:id])
+	song.update(params[:song])
+	redirect to ("/songs/#{song.id}")
+end
